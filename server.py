f35f80e010776d8a9a9f9919236f314c9927b7b9
Fatima <fatima.hammouri@gmail.com>
2021-02-28 11:42:09 +0000

    server working

--- server.py
+++ server.py
@@ -18,7 +18,8 @@ AWESOMENESS = [
 def start_here():
     """Home page."""
 
-    return "<!doctype html><html>Hi! This is the home page.</html>"
+    return """<!doctype html><html>Hi! This is the home page.</html>
+              For a special hello <a href="/hello" > click here </a>"""
 
 
 @app.route('/hello')
@@ -34,8 +35,32 @@ def say_hello():
       <body>
         <h1>Hi There!</h1>
         <form action="/greet">
-          What's your name? <input type="text" name="person">
+        <div>
+          <label>What's your name?</label>
+          <input type="text" name="person">
+
+          </div>
+
+          <div>
+          <label>Choose your favourite compliment:</label>
+            <div>
+            <select name="compliment">
+              <option name ="compliment" >fantastic</option>
+              <option name ="compliment" >smashing</option>
+              <option name ="compliment" >Amazing</option>
+              <option name ="compliment" >incredible</option>
+            </select>
+            </div>
+          </div>
+
+          <div>
+            <label>What do you enjoy doing ?</label>
+          <input type="text" name="hobby">
+          </div>
+
+          <div>
           <input type="submit" value="Submit">
+          </div>
         </form>
       </body>
     </html>
@@ -48,8 +73,10 @@ def greet_person():
 
     player = request.args.get("person")
 
-    compliment = choice(AWESOMENESS)
+    # compliment = choice(AWESOMENESS)
+    compliment = request.args.get("compliment")
 
+    hobby = request.args.get("hobby")
     return """
     <!doctype html>
     <html>
@@ -57,10 +84,10 @@ def greet_person():
         <title>A Compliment</title>
       </head>
       <body>
-        Hi, {}! I think you're {}!
+        Hi, {}! I think you're {} at {}!
       </body>
     </html>
-    """.format(player, compliment)
+    """.format(player, compliment, hobby)
 
 
 if __name__ == '__main__':
